a72c08d19a4affc0c37770c53f41c8265e273abc
Jonathan Rajotte <jraj@microsoft.com>
2017-03-15 12:21:43 -0400

    r

---
+++ linux-install-docker - Copy/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+# Check if docker is already installed.
+docker -v
+installationStatus=$(echo $?)
+
+if [ $installationStatus -eq 127 ] ; then
+    wget -qO- https://get.docker.com/ | sh
+fi
