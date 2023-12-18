418309a6c4617d95ac54a6224eebdc9e88bd9e20
Jonathan Rajotte <jraj@microsoft.com>
2023-12-18 17:15:28 -0500

    f

---
+++ wait-noFile/wait.ps1
@@ -0,0 +1,5 @@
+﻿$timer = Get-Date
+while (((Get-Date) - $timer).TotalSeconds -le 600) {
+    Write-Host "Time ealpsed " ((Get-Date) - $timer).Minutes "Minutes and" ((Get-Date) - $timer).Seconds "seconds"
+    Start-sleep  -Seconds 30
+}
